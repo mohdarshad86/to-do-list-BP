e92f749bbb3c4eb639480cdf2726fe5d5e993d9f
Mohd Arshad <mohdarshad8668@gmail.com>
2023-03-13 23:45:44 +0530

    added controllers and routes

---
+++ server/controller/todo-controller.js
@@ -0,0 +1,72 @@
+const Todo = require('../model/Todo.js');
+
+const addTodo = async (req, res) => {
+    try {
+        let data = req.body.data
+        const newTodo = await Todo.create({ data: data })
+        await newTodo.save();
+
+        return res.status(200).send(newTodo);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const getAllTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find({}).sort({ 'createdAt': -1 })
+
+        return res.status(200).send(todos);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const toggleTodoDone = async (req, res) => {
+    let paramId = req.params.id
+    try {
+        const todoRef = await Todo.findById(paramId);
+
+        const todo = await Todo.findOneAndUpdate(
+            { _id: paramId },
+            { done: !todoRef.done }
+        )
+
+        await todo.save();
+
+        return res.status(200).send(todo);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const updateTodo = async (req, res) => {
+    try {
+        let paramId = req.params.id
+        let data = req.body.data
+        await Todo.findOneAndUpdate(
+            { _id: paramId },
+            { data: data }
+        )
+
+        const todo = await Todo.findById(paramId);
+
+        return res.status(200).send(todo);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const deleteTodo = async (req, res) => {
+    try {
+        let paramId = req.params.id
+
+        const todo = await Todo.findByIdAndDelete(paramId)
+
+        return res.status(200).send(todo);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+module.exports = { addTodo, getAllTodos, toggleTodoDone, updateTodo, deleteTodo }
---
+++ server/routes/route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const { addTodo, getAllTodos, toggleTodoDone, updateTodo, deleteTodo } = require('../controller/todo-controller.js');
+
+const route = express.Router();
+
+
+route.post('/todos', addTodo)
+route.get('/todos', getAllTodos);
+route.get('/todos/:id', toggleTodoDone);
+route.put('/todos/:id', updateTodo);
+route.delete('/todos/:id', deleteTodo);
+
+
+module.exports = route;
